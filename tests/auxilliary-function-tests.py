9247ab779fc01938287e2f640d18b4ccce216e48
mikesiers <msiers@csu.edu.au>
2017-07-31 11:43:20 +1000

    bug: Fixed incorrect function call and unit test value.
    The function 'estimated_errors' was being mistakenly called 'estimated_error'. The unit test value which was corrected, was originally incorrect because of a rounding error.
    
    See GitHub Issue #2.

--- tests/auxilliary-function-tests.py
+++ tests/auxilliary-function-tests.py
@@ -9,25 +9,25 @@ class test_auxilliary_functions(unittest.TestCase):
     # Check that the correct output is produced for the three examples on page
     # 41 of C4.5 programs for machine learning:
     # (https://books.google.com.au/books?isbn=1558602380).
-    self.assertEqual(round(estimated_error(0, 6), 3), 1.238)
-    self.assertEqual(round(estimated_error(0, 9), 3), 1.284)
-    self.assertEqual(round(estimated_error(0, 1), 3), 0.750)
+    self.assertEqual(round(estimated_errors(0, 6), 3), 1.238)
+    self.assertEqual(round(estimated_errors(0, 9), 3), 1.285)
+    self.assertEqual(round(estimated_errors(0, 1), 3), 0.750)
 
     # Check that an error is raised if num_errors > num_records.
     with self.assertRaises(ValueError):
-      estimated_error(5, 4)
+      estimated_errors(5, 4)
 
     # Check that an error is raised if num_errors < 0.
     with self.assertRaises(ValueError):
-      estimated_error(-5, 4)
+      estimated_errors(-5, 4)
 
     # Check that an error is raised if num_records < 0.
     with self.assertRaises(ValueError):
-      estimated_error(5, -4)
+      estimated_errors(5, -4)
 
     # Check that an error is raised if confidence is not in [0, 50]
     with self.assertRaises(ValueError):
-      estimated_error(5, -4, 60)
+      estimated_errors(5, -4, 60)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
