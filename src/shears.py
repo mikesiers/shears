4abde35ebeb888278b6b49528e12bd13a367c73b
mikesiers <msiers@csu.edu.au>
2017-07-31 11:34:08 +1000

    src: Implemented estimated_errors function.
    The code was simply appropriated from a solution available from https://gist.github.com/DavidWalz/8538435. It has been credited in the docstring.
    
    See GitHub Issue #2.

---
+++ src/shears.py
@@ -0,0 +1,17 @@
+"""shears : Decision tree pruning functions.
+
+This module can be used to use various decision tree pruning functions to
+determine whether or not a decision tree node should be pruned.
+
+"""
+import scipy.stats
+
+def estimated_errors(num_errors, num_records, confidence=25):
+  """Calculates estimated errors using the Clopper-Pearson method.
+
+  This code has been appropriated from:
+  https://gist.github.com/DavidWalz/8538435
+  """
+  num_correct = num_records - num_errors
+  upper_bound = scipy.stats.beta.ppf(1 - alpha/100, num_errors+1, num_correct)
+  return upper_bound * num_records
