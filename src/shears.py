66b84ec6562088e1a42da0ee0e8a67976f3fa77c
mikesiers <msiers@csu.edu.au>
2017-07-31 12:33:33 +1000

    src: Added missing ValueError checks.
    The estimated_errors function was missing checks on the input. ValueErrors are now raised when a nonsensical value is passed to the function.
    
    See GitHub Issue #2.

--- src/shears.py
+++ src/shears.py
@@ -12,6 +12,17 @@ def estimated_errors(num_errors, num_records, confidence=25):
   This code has been appropriated from:
   https://gist.github.com/DavidWalz/8538435
   """
+
+  # Raise exceptions if any of the inputs are nonsensical.
+  if num_errors > num_records:
+    raise ValueError('There cannot be more errors than records.')
+  if num_errors < 0:
+    raise ValueError('There cannot be a negative number of errors.')
+  if num_records < 0:
+    raise ValueError('There cannot be a negative number of records.')
+  if confidence < 0 or confidence > 50:
+    raise ValueError('Confidence must be between 0 and 50 (both inclusive).')
+
   num_correct = num_records - num_errors
   significance = 1 - confidence / 100
   upper_bound = scipy.stats.beta.ppf(significance, num_errors+1, num_correct)
