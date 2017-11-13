97fc65b1cdb14d400b106fd4bd47749040e9121d
mikesiers <msiers@csu.edu.au>
2017-11-14 08:57:43 +1000

    docs: Added docstrings for pessimistic_prune function.
    This involved defining the arguments and the return value.
    
    See GitHub Issue #4.

--- src/shears.py
+++ src/shears.py
@@ -5,6 +5,7 @@ determine whether or not a decision tree node should be pruned.
 
 """
 import scipy.stats
+import wattle
 
 def estimated_errors(num_errors, num_records, confidence=25):
   """Calculates estimated errors using the Clopper-Pearson method.
@@ -40,3 +41,38 @@ def estimated_errors(num_errors, num_records, confidence=25):
   significance = 1 - confidence / 100
   upper_bound = scipy.stats.beta.ppf(significance, num_errors+1, num_correct)
   return upper_bound * num_records
+
+def pessimistic_prune(node, confidence=25):
+  """Returns a boolean describing whether or not the node should be pruned.
+
+  Args:
+    node (wattle.Node): The parent node to decide whether or not to prune.
+    confidence (int): The confidence level. For more information on what the
+      confidence level is, see Chapter 4 of the book:
+      C4.5 programs for machine learning.
+      https://books.google.com.au/books?isbn=1558602380
+
+  Returns:
+    (boolean): Whether or not this node should be pruned.
+  """
+
+  # Raise exceptions if any of the inputs are nonsensical.
+  if not all(child.is_leaf() child in node.children):
+    raise ValueError("Not all of the passed node's children are leaves.")
+
+  # Get the number of records and errors in node.
+  num_records = node.num_records()
+  num_errors = node.num_errors()
+  parent_errors = estimated_errors(num_errors, num_records, confidence)
+
+  # Sum the estimated errors for all of the children.
+  child_error_sum = 0
+  for child in node.children:
+    num_records = child.num_records()
+    num_errors = child.num_errors()
+    child_error_sum += estimated_errors(num_errors, num_records, confidence)
+
+  # If the estimated errors for the parent node is less than or equal to the
+  # estimated errors for the sum of all estimated errors of the children,
+  # return True, else return False.
+  return parent_errors <= child_error_sum
